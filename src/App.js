5b46df03b512263c08a04e613fbd473dda94137c
Daniil Kamalov <daniil.kamalov2001@mail.ru>
2024-03-26 17:21:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { boardsStore } from './consts/consts';
 
 function App() {
+  const [boards, setBoards] = useState(boardsStore);
+  const [currentBoard, setCurrentBoard] = useState(null);
+  const [currentItem, setCurrentItem] = useState(null);
+
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+    if (e.target.className === 'item') {
+        e.target.style.boxShadow = '0 4px 3px gray'
+    }
+  }
+
+  const dragLeaveHandler = (e) => {
+    e.target.style.boxShadow = 'none'
+  }
+
+  const dragStartHandler = (e, board, item) => {
+    setCurrentBoard(board);
+    setCurrentItem(item);
+  }
+
+  const dragEndHandler = (e) => {
+    e.target.style.boxShadow = 'none'
+  }
+
+  const dropHandler = (e, board, item) => {
+    e.preventDefault();
+    const currentIndex = currentBoard.items.indexOf(currentItem);
+    currentBoard.items.splice(currentIndex, 1);
+    const dropIndex = board.items.indexOf(item);
+    board.items.splice(dropIndex + 1, 0, currentItem);
+
+    setBoards(boards.map(b => {
+        if (b.id === board.id) {
+            return board;
+        }
+
+        if (b.id === currentBoard.id) {
+            return currentBoard;
+        }
+
+        return b;
+    }
+    ))
+  }
+
+  const dropCardHandler = (e, board) => {
+    board.items.push(currentItem);
+    const currentIndex = currentBoard.items.indexOf(currentItem);
+    currentBoard.items.splice(currentIndex, 1);
+
+    setBoards(boards.map(b => {
+        if (b.id === board.id) {
+            return board;
+        }
+
+        if (b.id === currentBoard.id) {
+            return currentBoard;
+        }
+
+        return b;
+    }
+    ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'> 
+        {boards.map(board =>
+            <div 
+                className='board'
+                onDragOver={e => dragOverHandler(e)}
+                onDrop={e => dropCardHandler(e, board)}
+            >
+                <div 
+                    className='board__title'
+                    key={board.id}
+                >
+                    {board.title}
+                </div>
+                {board.items.map(item =>
+                    <div
+                        onDragOver={e => dragOverHandler(e)}
+                        onDragLeave={e => dragLeaveHandler(e)}
+                        onDragStart={e => dragStartHandler(e, board, item)}
+                        onDragEnd={e => dragEndHandler(e)}
+                        onDrop={e => dropHandler(e, board, item)}
+                        draggable={true}
+                        className='item'
+                        key={item.id}
+                    >
+                        {item.title}
+                    </div>
+                )}
+            </div>  
+        )}
     </div>
   );
 }
---
+++ src/consts/consts.js
@@ -0,0 +1,32 @@
+export const boardsStore = [
+    {
+        id: 1,
+        title: 'Ожидание',
+        items: [
+            {
+                id: 1,
+                title: 'Ожидание'
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'В процессе',
+        items: [
+            {
+                id: 2,
+                title: 'Процесс'
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Готово',
+        items: [
+            {
+                id: 3,
+                title: 'Готово'
+            }
+        ]
+    },
+]
